8f0a01cc2736dea11beba6035e8d24d7e7b5ef2e
anselcheung <anselcheung@yahoo.com.sg>
2026-01-10 15:41:31 +0100

    wip

---
+++ src/analysis/pairwise_cosine_distance.py
@@ -0,0 +1,22 @@
+from scipy.spatial.distance import cdist
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import os
+import sys
+from pathlib import Path
+import numpy as np
+
+# Add parent directory to path for imports
+base_path = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(base_path))
+from src.data_utils import *
+
+data_path = os.path.join(base_path, "data", "data_final")
+csv_path = os.path.join(data_path, "final_dataset.csv")
+df = load_final_data_with_embeddings(csv_path, data_path, verbose=False)
+embeddings = np.stack(df.embedding.tolist())
+pairwise_cos_distance = cdist(embeddings, embeddings, metric="cosine")
+with open("pairwise_cos_dist.npy", "wb") as f:
+    np.save(f, pairwise_cos_distance)
